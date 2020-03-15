30f5533f16654d540217fbe475c472c3c84652df
bpetryshyn <bpetryshyn@hubspot.com>
2020-03-15 16:55:06 +0200

    Add morgan request logger middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('./data/redis');
 
 const express = require('express');
 const { json } = require('express');
+const morgan = require('morgan');
 
 const urlRouter = require('./api/routes/url');
 const log = require('./utils/log');
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(json);
+app.use(morgan('tiny'));
 
 app.use('/url', urlRouter);
 
