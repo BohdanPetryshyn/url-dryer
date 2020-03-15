407f5b5a9c196c12106fc883acbc598b15b60650
bpetryshyn <bpetryshyn@hubspot.com>
2020-03-15 15:03:52 +0200

    Use express body parser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv');
 require('./data/redis');
 
 const express = require('express');
+const { json } = require('express');
 
 const urlRouter = require('./api/routes/url');
 const log = require('./utils/log');
@@ -10,6 +11,7 @@ const log = require('./utils/log');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(json);
 
 app.use('/url', urlRouter);
 
