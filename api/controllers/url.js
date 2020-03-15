e98e2ca93b12ab719b0fdf098998c682b97bbda6
bpetryshyn <bpetryshyn@hubspot.com>
2020-03-15 16:57:40 +0200

    Add shortened url create functionality

--- api/controllers/url.js
+++ api/controllers/url.js
@@ -1,3 +1,15 @@
-exports.createUrl = (req, res, next) => {
-  res.end('Create URL');
+const redis = require('../../data/redis');
+const toUrlIdentifier = require('../../utils/toUrlIdentifier');
+
+exports.createUrl = async (req, res, next) => {
+  const { url } = req.body;
+
+  const lastUrlCounter = await redis.incr('lastUrl');
+  const shortenedUrl = toUrlIdentifier(lastUrlCounter);
+
+  await redis.hset(shortenedUrl, 'originalUrl', url);
+
+  res.status(200).json({
+    id: shortenedUrl,
+  });
 };
